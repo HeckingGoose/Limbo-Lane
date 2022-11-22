6f60a898825d841565f8867dd65b7a502e3482dc
Adam <a.bulman6453@gmail.com>
2022-11-22 20:14:55 +0000

    Minor changes

--- Assets/Oyster (Speech System)/Scripts/Oyster.cs
+++ Assets/Oyster (Speech System)/Scripts/Oyster.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using System.Data.OleDb;
 using System.Transactions;
 using UnityEngine.UI;
+using System.Linq.Expressions;
+using System;
 
 public class Oyster : MonoBehaviour
 {
     [SerializeField]
-    private ConsoleHandler consoleHandler;
-    [SerializeField]
     private string sus;
+    private string dataPath;
     void Start()
     {
-        
+        dataPath = Application.dataPath.Replace('/','\\') + @"\Resources\Oyster\JSON";
     }
     void Update()
     {
@@ -21,30 +22,6 @@ public class Oyster : MonoBehaviour
     }
     public void Speak(int characterIndex, int conversationIndex)
     {
-        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.dataPath + @"\Resources\Oyster\Oyster.accdb";
-        // Code currently stops at this point - may need to have an external service interface with the database, maybe an external C# script that's pre compiled?
-        OleDbConnection oysterDatabaseConnection = new OleDbConnection(connectionString);
-        OleDbCommand oysterDatabaseCommand = new OleDbCommand("SELECT * FROM character" + characterIndex.ToString() + "-conversations");
-        oysterDatabaseCommand.Connection = oysterDatabaseConnection;
-        try
-        {
-            oysterDatabaseConnection.Open();
-            try
-            {
-                OleDbDataReader output = oysterDatabaseCommand.ExecuteReader();
-                while (output.Read())
-                {
-                    sus += output.ToString() + '\n';
-                }
-            }
-            catch
-            {
-                consoleHandler.RaiseError("Database read Error", "Oyster,cs", "Failed to retrieve any data from the currently open database");
-            }
-        }
-        catch
-        {
-            consoleHandler.RaiseError("Connection Error", "Oyster.cs", "Failed to initialize a connection between the database and Oyster.");
-        }
+
     }
 }
